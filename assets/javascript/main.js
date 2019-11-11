7b2731d8d3a93414220de449fc6c257fa6a62d10
ThomasNGit <neronithomas1989@gmail.com>
2019-11-11 15:33:06 +0100

    Added the function which print on page the inputfield text

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,34 @@
 //creare uno script che permetta di stampare in pagina qullo che l'user inserisce nella finestra di imput
+$( document ).ready(function() {
+
+    //creo la funzione che cliccando sul pulsante il messaggio viene inviato
+    $(".sendmsg").click(function(){
+
+        console.log("ho cliccato");
+        
+        //salvo il valore dell'input utente
+        var messaggio = $(".usermsg").val();
+
+        console.log(messaggio);
+        
+
+        var elementmsg = $("#template .userinput").clone();
+
+        console.log(elementmsg);
+        
+
+        //inserisco nella copia del template l'input dell'utente
+        elementmsg.text(messaggio);
+
+        //appendo il testo nel div della chatbox
+        $(".chatbox").append(elementmsg);
+
+        console.log(elementmsg);
+        
+        //ripulisco il contenuto dell'inputfield
+        $("input").val("");
+
+    })
+
+
+})
