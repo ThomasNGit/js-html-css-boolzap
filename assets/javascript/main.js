0f4eefd78372c75bba5faf88d4367a4fbb18c9ff
ThomasNGit <neronithomas1989@gmail.com>
2019-11-13 12:09:27 +0100

    added automatic response after 1second

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,7 @@
 //creare uno script che permetta di stampare in pagina qullo che l'user inserisce nella finestra di imput
 $( document ).ready(function() {
 
+
     //creo la funzione che cliccando sul pulsante il messaggio viene inviato
     $(".sendmsg").click(function(){
 
@@ -14,6 +15,8 @@ $( document ).ready(function() {
 
         var elementmsg = $("#template #userinput").clone();
 
+        var risposta =$("#risposta").clone();
+
         console.log(elementmsg);
         
 
@@ -22,13 +25,61 @@ $( document ).ready(function() {
 
         //appendo il testo nel div della chatbox
         $(".chatbox").append(elementmsg);
+        
+        //creo funzione e intervallo che fa comparire una risposta
+        setTimeout(response, 1000);
 
-        console.log(elementmsg);
+        function response(){
+            $(".chatbox").append(risposta);
+        }
+        
         
         //ripulisco il contenuto dell'inputfield
         $("input").val("");
 
     })
 
+    //funzione per inviare il messaggio alla pressione del tasto invio
+    $(".usermsg").keypress(function(e){
+
+        if(e.which == 13){
+
+            console.log("ho cliccato");
+
+                    //salvo il valore dell'input utente
+            var messaggio = $(".usermsg").val();
+
+            console.log(messaggio);
+            
+
+            var elementmsg = $("#template #userinput").clone();
+
+            var risposta =$("#risposta").clone();
+
+            console.log(elementmsg);
+            
+
+            //inserisco nella copia del template l'input dell'utente
+            elementmsg.text(messaggio);
+
+            //appendo il testo nel div della chatbox
+            $(".chatbox").append(elementmsg);
+
+            //creo funzione e intervallo che fa comparire una risposta
+            setTimeout(response, 1000);
+
+            function response(){
+                $(".chatbox").append(risposta);
+            }
+           
+            //ripulisco il contenuto dell'inputfield
+            $("input").val("");
+        
+        }
+
+    })
 
 })
+
+
+
